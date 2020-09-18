8528c7b67380e49ae77547be42ca492b7628cdf6
NoteXYX <707997228@qq.com>
2020-09-18 16:32:07 +0800

    2020.01.14

--- predictGZ1110.py
+++ predictGZ1110.py
@@ -14,18 +14,18 @@ def test_batch_putin(test_lex, test_z, start_num=0, batch_size=16):
 
 def main():
     s = {
-        'nh1': 300,
-        'nh2': 300,
+        'nh1': 450,
+        'nh2': 450,
         'win': 3,
         'emb_dimension': 300,
-        'lr': 0.00005,
+        'lr': 0.0001,
         'lr_decay': 0.5,  #
         'max_grad_norm': 5,  #
         'seed': 345,  #
         'nepochs': 50,
         'batch_size': 16,
         'keep_prob': 1.0,
-        'check_dir': './checkpoints/kp20k_mycps_multisize_CNN_LSTM_attention_Adam_0.0001_16',
+        'check_dir': './checkpoints/GZ_mycps_Adam_0.0001_16/kp20k',
         'display_test_per': 1,  #
         'lr_decay_per': 5  #
     }
@@ -35,8 +35,8 @@ def main():
     # emb_file = 'CNTN/data/inspec_wo_stem/embedding.pkl'
     # data_set_file = 'data/ACL2017/krapivin/krapivin_t_a_GZ_data_set.pkl'
     # emb_file = 'data/ACL2017/krapivin/krapivin_t_a_GZ_embedding.pkl'
-    data_set_file = 'data/ACL2017/inspec/inspec_t_a_allwords_data_set.pkl'
-    emb_file = 'data/ACL2017/ACL2017_t_a_embedding.pkl'
+    data_set_file = 'data/ACL2017/kp20k/kp20k_t_a_data_set.pkl'
+    emb_file = 'data/ACL2017/kp20k/kp20k_t_a_embedding.pkl'
     # train_set, test_set, dic, embedding = load.atisfold(data_set_file, emb_file)
     print('loading dataset.....')
     train_set, valid_set, test_set, dic, embedding = load.atisfold_ACL2017(data_set_file, emb_file)
@@ -63,7 +63,7 @@ def main():
         )
 
         checkpoint_dir = s['check_dir']
-        logfile = open(str(s['check_dir']) + '/predict_log.txt', 'a', encoding='utf-8')
+        logfile = open(str(s['check_dir']) + '/predict_log_NEW.txt', 'a', encoding='utf-8')
         saver = tf.train.Saver(tf.all_variables())
         ckpt = tf.train.get_checkpoint_state(checkpoint_dir)
         if ckpt and ckpt.model_checkpoint_path:
--- tools.py
+++ tools.py
@@ -39,46 +39,75 @@ def contextwin_2(ls,win):
 
 def getKeyphraseList(l):
     res, now= [], []
+    singleKW = []
+    moreKP = []
     for i in range(len(l)):
         if l[i] != 0:
             now.append(str(i))
         if l[i] == 0 or i == len(l) - 1:
             if len(now) != 0:
                 res.append(' '.join(now))
+                if len(now) == 1:
+                    singleKW.append(now[0])
+                else:
+                    moreKP.append(' '.join(now))
             now = []
-    return set(res)
+    return set(res), set(singleKW), set(moreKP)
 
 def getKeyphraseList_top(l, top_num):
     res, now= [], []
+    singleKW = []
+    moreKP = []
     for i in range(len(l)):
         if l[i] != 0:
             now.append(str(i))
         if l[i] == 0 or i == len(l) - 1:
             if len(now) != 0:
                 res.append(' '.join(now))
+                if len(now) == 1:
+                    singleKW.append(now[0])
+                else:
+                    moreKP.append(' '.join(now))
                 if len(res) >= top_num:
                     break
             now = []
-    return set(res)
+    return set(res), set(singleKW), set(moreKP)
 
 def conlleval(predictions, groundtruth):
     assert len(predictions) == len(groundtruth)
     res = {}
     all_cnt, good_cnt = len(predictions), 0
     p_cnt, r_cnt = 0, 0
+    goodSingleCnt, goodMoreCnt = 0, 0
+    singlePreCnt, singleRecallCnt = 0, 0
+    morePreCnt, moreRecallCnt = 0, 0
     for i in range(all_cnt):
         # print i
         # if all(predictions[i][0:len(groundtruth[i])] == groundtruth[i]) == True:
         #     good_cnt += 1
-        pKeyphraseList = getKeyphraseList(predictions[i][0:len(groundtruth[i])])
-        gKeyphraseList = getKeyphraseList(groundtruth[i])
+        pKeyphraseList, pSingleKpList, pMoreKpList = getKeyphraseList(predictions[i][0:len(groundtruth[i])])
+        gKeyphraseList, gSingleKpList, gMoreKpList = getKeyphraseList(groundtruth[i])
         for p in pKeyphraseList:
             for g in gKeyphraseList:
                 if p == g:
                     good_cnt += 1
                     break
+        for p in pSingleKpList:
+            for g in gSingleKpList:
+                if p == g:
+                    goodSingleCnt += 1
+                    break
+        for p in pMoreKpList:
+            for g in gMoreKpList:
+                if p == g:
+                    goodMoreCnt += 1
+                    break
         p_cnt += len(pKeyphraseList)  #######################
         r_cnt += len(gKeyphraseList)  #######################
+        singlePreCnt += len(pSingleKpList)
+        singleRecallCnt += len(gSingleKpList)
+        morePreCnt += len(pMoreKpList)
+        moreRecallCnt += len(gMoreKpList)
         # if len(pKeyphraseList) != 0:
         #     p_cnt += 1
         # if len(gKeyphraseList) != 0:
@@ -88,6 +117,12 @@ def conlleval(predictions, groundtruth):
     res['p'] = 1.0*good_cnt/p_cnt
     res['r'] = 1.0*good_cnt/r_cnt
     res['f'] = 2.0*res['p']*res['r']/(res['p']+res['r'])
+    res['SinglePre'] = 1.0 * goodSingleCnt/singlePreCnt
+    res['SingleRecall'] = 1.0 * goodSingleCnt/singleRecallCnt
+    res['SingleF1'] = 2.0 * res['SinglePre']*res['SingleRecall']/(res['SinglePre']+res['SingleRecall'])
+    res['MorePre'] = 1.0 * goodMoreCnt / morePreCnt
+    res['MoreRecall'] = 1.0 * goodMoreCnt / moreRecallCnt
+    res['MoreF1'] = 2.0 * res['MorePre'] * res['MoreRecall'] / (res['MorePre'] + res['MoreRecall'])
     return res
 
 def conlleval_top(predictions, groundtruth, top_num):
@@ -95,20 +130,43 @@ def conlleval_top(predictions, groundtruth, top_num):
     res = {}
     all_cnt, good_cnt = len(predictions), 0
     p_cnt, r_cnt = 0, 0
+    goodSingleCnt, goodMoreCnt = 0, 0
+    singlePreCnt, singleRecallCnt = 0, 0
+    morePreCnt, moreRecallCnt = 0, 0
     for i in range(all_cnt):
-        pKeyphraseList = getKeyphraseList_top(predictions[i][0:len(groundtruth[i])], top_num)
-        gKeyphraseList = getKeyphraseList(groundtruth[i])
+        pKeyphraseList, pSingleKpList, pMoreKpList = getKeyphraseList_top(predictions[i][0:len(groundtruth[i])], top_num)
+        gKeyphraseList, gSingleKpList, gMoreKpList = getKeyphraseList(groundtruth[i])
         for p in pKeyphraseList:
             for g in gKeyphraseList:
                 if p == g:
                     good_cnt += 1
                     break
+        for p in pSingleKpList:
+            for g in gSingleKpList:
+                if p == g:
+                    goodSingleCnt += 1
+                    break
+        for p in pMoreKpList:
+            for g in gMoreKpList:
+                if p == g:
+                    goodMoreCnt += 1
+                    break
         p_cnt += len(pKeyphraseList)  #######################
         r_cnt += len(gKeyphraseList)  #######################
+        singlePreCnt += len(pSingleKpList)
+        singleRecallCnt += len(gSingleKpList)
+        morePreCnt += len(pMoreKpList)
+        moreRecallCnt += len(gMoreKpList)
     res['a'] = 1.0*good_cnt/all_cnt
     res['p'] = 1.0*good_cnt/p_cnt
     res['r'] = 1.0*good_cnt/r_cnt
     res['f'] = 2.0*res['p']*res['r']/(res['p']+res['r'])
+    res['SinglePre'] = 1.0 * goodSingleCnt/singlePreCnt
+    res['SingleRecall'] = 1.0 * goodSingleCnt/singleRecallCnt
+    res['SingleF1'] = 2.0 * res['SinglePre']*res['SingleRecall']/(res['SinglePre']+res['SingleRecall'])
+    res['MorePre'] = 1.0 * goodMoreCnt / morePreCnt
+    res['MoreRecall'] = 1.0 * goodMoreCnt / moreRecallCnt
+    res['MoreF1'] = 2.0 * res['MorePre'] * res['MoreRecall'] / (res['MorePre'] + res['MoreRecall'])
     return res
 
 
